739067ae899cbfa49b3fab52e08488e7264e4eae
azizaouad <72755717+azizaouad@users.noreply.github.com>
2023-09-02 12:21:54 +0100

    adding hooks (beforeEach and afterEach)

--- cypress/e2e/testcase/cormartest.cy.js
+++ cypress/e2e/testcase/cormartest.cy.js
@@ -1,16 +1,28 @@
 ///<reference types="cypress"/>
 import ComarTest from "../../pages/ComarTest";
 
+beforeEach(() => {
+  const ln = new ComarTest();
+  ln.navigate("https://www.comar.tn/");
+  ln.clickActualites();
+});
+
+afterEach(() => {
+  cy.end();
+});
+
 describe("Comar Automation", () => {
-  it("Automates the required actions", () => {
+  it("check the word", () => {
     const ln = new ComarTest();
-    ln.navigate("https://www.comar.tn/");
-    ln.clickActualites();
 
     const word = "Actualités";
     ln.CheckText(word);
+  });
 
-    // Actualité html
+  // Actualité html
+
+  it("check the text", () => {
+    const ln = new ComarTest();
     ln.search("COMAR Assurances partenaire");
 
     const text = "COMAR Assurances partenaire du Semi-Marathon Ulysse Djerba";
